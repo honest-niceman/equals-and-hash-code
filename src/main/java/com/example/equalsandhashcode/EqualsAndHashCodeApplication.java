f3784c17fdd2fd117554d17ddd11c235c7529938
Georgii Vlasov <honest.niceman@gmail.com>
2023-06-16 09:51:13 +0400

    Init commit

---
+++ src/main/java/com/example/equalsandhashcode/EqualsAndHashCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.equalsandhashcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqualsAndHashCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EqualsAndHashCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/equalsandhashcode/EqualsAndHashCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.equalsandhashcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqualsAndHashCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
