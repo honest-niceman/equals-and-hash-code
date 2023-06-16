adc921c7eecfc14f5c3887b93ef6aefcb31c694e
Georgii Vlasov <honest.niceman@gmail.com>
2023-06-16 10:14:44 +0400

    Add addToHashBasedCollectionCausesInitializationTest

--- src/test/java/com/example/equalsandhashcode/EqualsAndHashCodeApplicationTests.java
+++ src/test/java/com/example/equalsandhashcode/EqualsAndHashCodeApplicationTests.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootTest
 class EqualsAndHashCodeApplicationTests {
 
@@ -22,12 +25,21 @@ class EqualsAndHashCodeApplicationTests {
     void contextLoads() {
     }
 
-    @Sql(scripts = "insert-dog-and-cat.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = "delete-dog-and-cat.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(scripts = "differentEntitiesTest/insert-dog-and-cat.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "differentEntitiesTest/delete-dog-and-cat.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Test
     public void differentEntitiesTest() {
         Dog dog = dogRepository.findById(1L).orElseThrow();
         Cat cat = catRepository.findById(1L).orElseThrow();
         Assertions.assertNotEquals(cat, dog);
+    }
+
+    @Test
+    @Sql(scripts = "addToHashBasedCollectionCausesInitializationTest/insert-dog.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "addToHashBasedCollectionCausesInitializationTest/delete-dog.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void addToHashBasedCollectionCausesInitializationTest() {
+        Dog proxyDog = dogRepository.getReferenceById(1L);
+        Set<Dog> dogs = new HashSet<>();
+        dogs.add(proxyDog);
     }
 }
